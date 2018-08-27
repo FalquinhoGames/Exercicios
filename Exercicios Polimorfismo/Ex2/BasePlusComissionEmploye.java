8861916d411c880185fa1eb137f5206934a12eda
Filipe Ferreira Falco <35337326+FalquinhoGames@users.noreply.github.com>
2018-08-27 16:06:05 -0400

    Update BasePlusComissionEmploye.java

--- Exercicios Polimorfismo/Ex2/BasePlusComissionEmploye.java
+++ Exercicios Polimorfismo/Ex2/BasePlusComissionEmploye.java
@@ -1,36 +1,49 @@
 package Ex2;
 
 public class BasePlusComissionEmploye {
-	ComissionEmployee c1 = new ComissionEmployee();
 
 	/*************
 	 * ATRIBUTOS *
 	 *************/
-	private ComissionEmployee firstName;
-	private ComissionEmployee lastName;
-	private ComissionEmployee socialSecurityNumber;
-	private ComissionEmployee grossSales;
-	private ComissionEmployee comissionRate;
 	private double baseSalary;
-
-	public BasePlusComissionEmploye(ComissionEmployee firstName, ComissionEmployee lastName, ComissionEmployee socialSecurityNumber, ComissionEmployee grossSales,
-									ComissionEmployee comissionRate, double baseSalary) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.socialSecurityNumber = socialSecurityNumber;
-		this.grossSales = grossSales;
-		this.comissionRate = comissionRate;
+	private ComissionEmployee comissionEmployee;
+	
+	/**************
+	 * CONSTRUTOR *
+	 **************/
+	public BasePlusComissionEmploye(double baseSalary, ComissionEmployee comissionEmployee) {
 		this.baseSalary = baseSalary;
+		this.comissionEmployee = comissionEmployee;
+	}
+
+	public String getFirstName() {
+		return comissionEmployee.getFirstName();
+	}
+	
+	public String getLastName() {
+		return comissionEmployee.getLastName();
+	}
+
+	public String getSocialSecurityNumber() {
+		return comissionEmployee.getSocialSecurityNumber();
+	}
+
+	public double getGrossSales() {
+		return comissionEmployee.getGrossSales();
+	}
+
+	public double getComissionRate() {
+		return comissionEmployee.getComissionRate();
 	}
 
 	/***********
 	 * METODOS *
 	 ***********/
 	public double earnings() {
-		return c1.earnings() + baseSalary;
+		return comissionEmployee.earnings() + baseSalary;
 	}
 	
 	public String toString() {
-		return "Nome: " + firstName + " " + lastName + "\nNumero do seguro social: " + socialSecurityNumber + "\nVendas Brutas: " + grossSales + "\nTaxa de comissao: " + comissionRate + "\nLucros: " + earnings() + "\nSalario base: " + baseSalary;
+		return "Nome: " + getFirstName() + " " + getLastName() + "\nNumero do seguro social: " + getSocialSecurityNumber() + "\nVendas Brutas: " + getGrossSales() + "\nTaxa de comissao: " + getComissionRate() + "\nLucros: " + earnings() + "\nSalario base: " + baseSalary;
 	}
 }
