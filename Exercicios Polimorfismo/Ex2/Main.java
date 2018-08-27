1e6e1efbe9f3db3136976965777d25917c5bca2d
Filipe Ferreira Falco <35337326+FalquinhoGames@users.noreply.github.com>
2018-08-27 16:06:46 -0400

    Update Main.java

--- Exercicios Polimorfismo/Ex2/Main.java
+++ Exercicios Polimorfismo/Ex2/Main.java
@@ -2,9 +2,9 @@ package Ex2;
 
 public class Main {
 	public static void main(String[] args) {
-		ComissionEmployee pessoa = new ComissionEmployee();
-		BasePlusComissionEmploye pessoaMais= new BasePlusComissionEmploye(pessoa, pessoa, pessoa, pessoa, pessoa, 2000);
-
-		System.out.printf("Pessoa:\n%s\nPessoa com mais informações:\n%s",pessoa.toString(), pessoaMais.toString());
+		ComissionEmployee pessoa = new ComissionEmployee("Filipe", "Ferreira Falco", "1110800", 0.10, 1000);
+		BasePlusComissionEmploye pessoaMais= new BasePlusComissionEmploye(2000, pessoa);
+		
+		System.out.printf("Pessoa:\n%s\n\nPessoa com mais informações:\n%s",pessoa.toString(), pessoaMais.toString());
 	}
 }
