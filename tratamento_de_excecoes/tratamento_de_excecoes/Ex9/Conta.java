73ec5b45b4c2ae9cfaecabb9b0a900a22b752a1d
201819060497 <201819060497@lab01-45.dominio.facom.ufms.br>
2018-09-06 16:43:56 -0400

    aoidj

---
+++ tratamento_de_excecoes/tratamento_de_excecoes/Ex9/Conta.java
@@ -0,0 +1,21 @@
+public class Conta {
+	private double Limite;
+	
+	/***********
+	 * METODOS *
+	 ***********/
+	public void saca(double dinheiro) {
+		
+	}
+	
+	public void deposita(double dinheiro) {
+		
+	}
+	
+	/*******************
+	 * GETTER E SETTER *
+	 *******************/
+	public void setLimite() {
+		
+	}
+}
