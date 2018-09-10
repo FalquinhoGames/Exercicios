575851da8b9aecce8b306f7724ee404822480701
FalquinhoGames <falcoefilipe@hotmail.com>
2018-09-09 22:50:49 -0400

    Ex9 da lista "tratamento_de_excessoes concluido!"

--- tratamento_de_excecoes/tratamento_de_excecoes/Ex9/Conta.java
+++ tratamento_de_excecoes/tratamento_de_excecoes/Ex9/Conta.java
@@ -1,20 +1,38 @@
+package Ex9;
+
 public class Conta {
-	private double Limite;
-	
+	private double limite;
+	private double dinheiro;
+
+	public Conta() {
+	}
+
 	/***********
 	 * METODOS *
 	 ***********/
 	public void saca(double dinheiro) {
-		
-	}
+	    try {
+            if (dinheiro > limite){
+                throw new ContaException(limite, dinheiro);
+            } else{
+                limite = limite - dinheiro;
+                System.out.println("Saldo: " + limite);
+            }
+        }catch (ContaException e) {
+            System.out.println("não é possivel sacar essa quantia!");
+        }
+    }
 	
 	public void deposita(double dinheiro) {
-		
-	}
+	    this.dinheiro = dinheiro;
+        System.out.println("Depositou: " + dinheiro);
+    }
 	/*******************
 	 * GETTER E SETTER *
 	 *******************/
-	public void setLimite() {
-		
+	public void setLimite(double limite) {
+	    this.limite = limite;
+	    this.limite += this.dinheiro;
+        System.out.println("limite agora: " + this.limite);
 	}
 }
--- tratamento_de_excecoes/tratamento_de_excecoes/Ex9/ContaException.java
+++ tratamento_de_excecoes/tratamento_de_excecoes/Ex9/ContaException.java
@@ -1,3 +1,17 @@
-public class ContaException {
+package Ex9;
 
+public class ContaException extends Exception{
+
+    private double limite;
+    private double dinheiro;
+
+    public ContaException(double limite, double dinheiro) {
+        this.limite = limite;
+        this.dinheiro = dinheiro;
+    }
+
+    @Override
+    public String toString() {
+        return "não é possivel sacar essa quantia!";
+    }
 }
