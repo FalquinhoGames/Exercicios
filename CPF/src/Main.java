9cb57b1db24c71f098cb91e2f621d6a16bebe41e
FalquinhoGames <falcoefilipe@hotmail.com>
2018-09-11 23:41:53 -0400

    Teste do CPF

---
+++ CPF/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String cpf;
+        char []vetor = new char[14];
+
+        System.out.println("Digite seu CPF");
+        cpf = entrada.nextLine();
+
+
+    }
+}
